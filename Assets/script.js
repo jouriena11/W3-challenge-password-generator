b13046410b27615eaa6b98f0fd7da5b005d4a394
jouriena11 <117335220+jouriena11@users.noreply.github.com>
2022-12-01 10:41:58 +1100

    update javascript codes + edit comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,5 @@
 // Assignments of variables used in password generation
 var char26 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-// Convert char26 uppercase string into lowercase string by using toLowerCase() string method
-var char26Lower = char26.toLowerCase();
-
-// Convert uppercase and lowercase strings into an array by using Array.from method, as this is more efficient than having to type the alphabet array ["A", "B", "C", and so on] manually.
-var charUpper = Array.from(char26);
-var charLower = Array.from(char26Lower)
 var charNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var charSpecial = [
   '@',
@@ -34,42 +27,49 @@ var charSpecial = [
   '.'
 ];
 
+// Convert char26 uppercase string into lowercase string by using toLowerCase() string method
+var char26Lower = char26.toLowerCase();
+
+// Convert uppercase and lowercase strings into an array by using Array.from method, as this is more efficient than having to type the alphabet array ["A", "B", "C", and so on] manually.
+var charUpper = Array.from(char26);
+var charLower = Array.from(char26Lower)
+
 // Assignment code
 
 var generateBtn = document.querySelector("#generate");
 
-function confirm_Length() {
-  confirmLength = prompt("Please specify the length of your password between 8-128 characters");
-}
-
-var confirmLength = prompt("Please specify the length of your password between 8-128 characters");
-
 function generatePassword() {
-  confirm_Length();
+  confirmLength = prompt("Please specify the length of your password between 8-128 characters");
   console.log(confirmLength)
-  
-  /* Loop if answer is outside the parameters -- if confirmLength value is less than 8 or greater than 128
-    - an alert message would appear
-    - user is asked to re-specify password length
-    - Once a permitted value is entered, a password length confirmation message appears before proceeding to confirming password criteria.
+
+  /* Expected behavior: If a user enters an incorrect value, e.g. a number that's less than 8,  a number that's greater than 128, 
+  a negative number, or a blank, then an alert message would appear and exit once the user clicks 'ok.'
+
+  If the user enters a correct value of the password link, a confirmation message would appear before proceeding to the password
+  criteria specification.
+
+  Although [while] loop can be used to automatically re-prompt and have the user re-enter the password length, I was advised that
+  this could negatively affect user experience, especially mobile user experience. So [if] conditional statement is deemed more
+  appropriate in this context.
   */
+ 
+  if(confirmLength < 8 || confirmLength > 128){
+    alert("Error: Your password must be between 8-128 characters. Please try again");
+    return
+  }
+    alert("Your password will have " + confirmLength + " characters");
   
-  while (confirmLength < 8 || confirmLength > 128) {
-      alert("Error: Your password must be between 8-128 characters. Please try again")
-      confirm_Length();
-      }
-      alert("Your password will have " + confirmLength + " characters");
+  // Determine password criteria -- 'Ok' = true; 'Cancel' = false
   
-  /* Determine password criteria
-      - 'Ok' = true
-      - 'Cancel' = false  
-  */
       var confirmUpper = confirm("Would you like to include uppercase characters in your password? Click 'OK' if you do.");
       var confirmLower = confirm("Would you like to include lowercase characters in your password? Click 'OK' if you do.");
       var confirmNumber = confirm("Would you like to include numbers in your password? Click 'OK' if you do.");
       var confirmSpecial = confirm("Would you like to include special characters in your password? Click 'OK' if you do.");
   
-  // Loop if answer is outside the parameters -- a user must select (i.e., select "Ok") a minimum of 1 criteria
+  /* If the user answers outside the parameters (i.e., not selecting any criteria) an alert message would appear, asking the
+  user to re-specify the criteria. Here, the [while] loop is appropriate as a user probably wouldn't want to re-enter the 
+  password length again. */
+
       while (confirmUpper === false && confirmLower === false && confirmNumber === false && confirmSpecial === false) {
       alert("Error! You must specify at least 1 parameter to generate a password");
       var confirmUpper = confirm("Would you like to include uppercase characters in your password? Click 'OK' if you do.");
@@ -78,8 +78,11 @@ function generatePassword() {
       var confirmSpecial = confirm("Would you like to include special characters in your password? Click 'OK' if you do.");
       }
 
-  // Assign an action to the password parameters -- do the following if condition stack on top of one another?
-  var passwordCombo = []
+  /* Assign an action to the password parameters -- to include all arrays conditional to the user's true/false value in each
+  confirm() method above, each variable has its own conditional statement and will be concatenated in passwordCombo variable 
+  with concat() if the conditional statement is true. */
+  
+  var passwordCombo = [] // As concat() method only works on array, var passwordCombo is assigned an empty array and concatenated with the variable(s) conditional statement that returns true.
 
   if (confirmUpper) {
     passwordCombo = passwordCombo.concat(charUpper)
@@ -103,14 +106,18 @@ function generatePassword() {
 
   console.log(passwordCombo)
 
-  /* Still don't understand the mechanism of this loop 
-  - what does i refer to in this loop?
-  - what's the function of [ ] in the 2nd line of this for loop? An array?
+
+  /* Generate a random with the 'for' loop - the expressions in the loop behaves as a counter, dictating how many times the 
+  code that follows (i.e., the user's specify password length) will run.
+  
+  We want the final random generated password to be a string, hence a var randomPassword is declared and assigned an empty 
+  string value. The code within the 'for' loop will thereafter replace the randomPassword empty string with the newly created
+  value, and the newly created value will be replaced by the newly created value, the process which is repeated for the same
+  number of times as the password length.
   */
   
   var randomPassword = ""
 
-// i determines the number of times the code is run
   for (var i = 0; i < confirmLength; i++) {
     randomPassword = randomPassword + passwordCombo[Math.floor(Math.random() * passwordCombo.length)]
     console.log(randomPassword)
@@ -120,6 +127,7 @@ function generatePassword() {
 }
 
 // Write password to the #password input, i.e, replace placeholder in the <textarea> with ID="password" with the generated password
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
